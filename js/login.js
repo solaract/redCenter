08732f03b8239ab1d4bb96b5b9358860f20c3d37
zxy <304246725@qq.com>
2015-06-14 23:29:49 +0800

    footer OK

--- js/login.js
+++ js/login.js
@@ -1,21 +1,30 @@
 /**
  * Created by zxy on 2015/6/11.
  */
-var user = $('#inputUser')[0];
-var pass = $('#inputPassword')[0];
-var check = new form_check({
+var user = $('#inputUser');
+var pass = $('#inputPassword');
+var forget = $('.form_forget');
+var comp = $('.complete')[0];
+var form_ask = $('#form_ask')[0];
+forget.bind('click',function(){
+    comp.style.display = 'block';
+    form_ask.style.display = 'block';
+});
+//登陆验证
+var login_check = new form_check({
     form:'#form_login',
     check:[function(){
         var reg = /s?\d+/;
         var value = user.value;
         if(reg.test(value)&&value.length === 10&&pass.value!==''){
-            console.log('true');
+//            console.log('true');
             return true;
         }
         else{
-            console.log('false');
+//            console.log('false');
             return false;
         }
     }]
 });
-check.run();
+
+login_check.run();

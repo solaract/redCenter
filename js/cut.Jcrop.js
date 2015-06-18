e0123d6b72f29e05e2f199fc24a17bec68b1db8c
zxy <304246725@qq.com>
2015-06-18 14:09:47 +0800

    头像裁剪及登陆界面完善

--- js/cut.Jcrop.js
+++ js/cut.Jcrop.js
@@ -1,13 +1,27 @@
 // jQuery(function($){
 var face = $('.left_face');
+
+//点击头像弹出侧栏
 face.popover({
     trigger:'click',
-    template:'<div class="popover font_fa" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div id="cut"><form id="up_form" action="#" enctype="multipart/form-data" method="post"><p>选择图片（支持jpg、png、gif）</p><div class="up_content"><div class="up_box"><div id="up_txt"></div><button class="up_button btn btn-primary">浏览..</button><input id="up_file" name="file" type="file"></div><input class="btn btn-primary" type="submit" value="下一步"></div></form></div></div>'
+    template:'<div class="popover font_fa" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div id="cut"><form id="up_form" action="#" enctype="multipart/form-data" method="post"><p>选择图片（支持jpg、png、gif）</p><div class="up_content"><div class="up_box"><div id="up_txt"></div><button class="up_button btn btn-primary">浏览..</button><input id="up_file" name="file" type="file" accept="image/*"></div><input class="btn btn-primary" type="submit" value="下一步"></div></form></div></div>'
 //<form id="up_form" action="#" enctype="multipart/form-data" method="post"><div class="up_content"><div class="up_box"><div id="up_txt"></div><div class="up_button">浏览..</div><input id="up_file" name="file" type="file"></div><input class="up_submit" type="submit" value="上传"></div></form>
 });
+
+//初始化侧栏
 face.popover('show');
-var up_txt = $('#up_txt');
-var cut = $('#cut');
+face.bind('hide.bs.popover',function(){
+    $('#up_form').css('display','block');
+    $('#cut_face').css('display','none');
+    $('#up_file').val('');
+    $up_txt.html('');
+});
+
+var $up_form = $('#up_form');
+var $up_txt = $('#up_txt');
+var $cut = $('#cut');
+
+//上传图片input验证
 $('#up_file').bind('change',function(){
     var file = this.value;
 //    console.log(file);
@@ -15,115 +29,183 @@ $('#up_file').bind('change',function(){
     var jpg = file.match(/[^\\]+.jpg/);
     var png = file.match(/[^\\]+.png/);
     var gif = file.match(/[^\\]+.gif/);
-    if(jpg!=null){
+    if(jpg!==null){
 //        up_txt.innerHTML=jpg[0];
-        up_txt.html(jpg[0]);
+        $up_txt.html(jpg[0]);
     }
-    else if(png!=null){
+    else if(png!==null){
 //        up_txt.innerHTML=png[0];
-        up_txt.html(png[0]);
+        $up_txt.html(png[0]);
     }
-    else if(gif!=null){
+    else if(gif!==null){
 //        up_txt.innerHTML=gif[0];
-        up_txt.html(gif[0]);
+        $up_txt.html(gif[0]);
+    }
+    else if(file === ''){
+        $up_txt.html('');
     }
     else{
         alert('请选择正确的图片格式')
     }
 });
+//上传图片表单验证
 var file_check = new form_check({
     form:'#up_form',
     check:[function(){
-        if(up_txt.html()==''||up_txt.html()==null){
+        //上传图片不能为空
+        if($up_txt.html()==''||$up_txt.html()==null){
             alert('请先选择图片');
+            return false;
+        }
+        var file = $('#up_file').val();
+        var jpg = file.match(/[^\\]+.jpg/);
+        var png = file.match(/[^\\]+.png/);
+        var gif = file.match(/[^\\]+.gif/);
+        if(!jpg&&!png&&!gif){
+            alert('请选择正确的图片格式');
             return false;
         }
         return true;
     }],
+    //ajax上传，返回图片url，回调进入预裁剪界面
     success:function(){
-
+//        alert(1);
+        $up_form.css('display','none');
+        var img_url = "img/face.jpg";
+        if($('#cut_face')[0] === undefined){
+            $cut.append('<div id="cut_face">'+
+                '<img src='+img_url+' id="target" alt="[Jcrop Example]" />'+
+                '<div id="preview-pane">'+
+                '<div class="preview-container">'+
+                '<img src='+img_url+' class="jcrop-preview" alt="Preview" />'+
+                '</div>'+
+                '</div>'+
+                '<div class="clearfix"></div>'+
+                '<form id="cut_form" action="#" method="post">'+
+                '<input type="hidden" id="x" name="x">'+
+                '<input type="hidden" id="y" name="y">'+
+                '<input type="hidden" id="w" name="w">'+
+                '<input type="hidden" id="h" name="h">'+
+                '<input class="cxbtn btn btn-primary" type="submit" value="上传头像">'+
+                '<input class="cut_close btn btn-primary" type="button" value="上一步">'+
+                '</form>'+
+                '</div>');
+            cut_img();
+            $('.cut_close').bind('click',function(){
+                $('#cut_face').css('display','none');
+                $up_form.css('display','block');
+//                return false;
+            });
+            var cut_check = new form_check({
+                form:'#cut_form',
+                check:[function(){
+                    var x = typeof parseInt($('#x').val()) === 'number';
+                    var y = typeof parseInt($('#y').val()) === 'number';
+                    var w = typeof parseInt($('#w').val()) === 'number';
+                    var h = typeof parseInt($('#h').val()) === 'number';
+                    if(x&&y&&w&&h){
+//                        alert(1);
+                        return true;
+                    }
+                    return false;
+                }]
+            });
+            cut_check.run();
+        }
+        else{
+            $('#target').attr('src',img_url);
+            $('.jcrop-preview').attr('src',img_url);
+            $('#cut_face').css('display','block');
+        }
+        $('.arrow').css('top','20%');
+        return false;
     }
 });
 file_check.run();
+face.popover('hide');
     // Create variables (in this scope) to hold the API and image size
-//    var jcrop_api,
-//        boundx,
-//        boundy,
-//
-//        // Grab some information about the preview pane
-//        $preview = $('#preview-pane'),
-//        $pcnt = $('#preview-pane .preview-container'),
-//        $pimg = $('#preview-pane .preview-container img'),
-//        $tar = $('#target'),
-//        holder = $('.jcrop-holder')[0],
-//
-//        xsize = $pcnt.width(),
-//        ysize = $pcnt.height(),
-//        tar_wid = $tar.width(),
-//        tar_hei = $tar.height();
-//
-//    // if(tar_wid > tar_hei){
-//    //   $tar.css('width','300px');
-//    // }
-//    // else{
-//    //   $tar.css('height','300px');
-//    // }
-//    // console.log('init',[xsize,ysize]);
-//
-//    $tar.Jcrop({
+var cut_img = function(){
+        var jcrop_api,
+        boundx,
+        boundy,
+
+        // Grab some information about the preview pane
+        $preview = $('#preview-pane'),
+        $pcnt = $('#preview-pane .preview-container'),
+        $pimg = $('#preview-pane .preview-container img'),
+        $tar = $('#target'),
+        holder = $('.jcrop-holder')[0],
+
+        xsize = $pcnt.width(),
+        ysize = $pcnt.height(),
+        tar_wid = $tar.width(),
+        tar_hei = $tar.height();
+
+    // if(tar_wid > tar_hei){
+    //   $tar.css('width','300px');
+    // }
+    // else{
+    //   $tar.css('height','300px');
+    // }
+    // console.log('init',[xsize,ysize]);
+    //裁剪设置
+    $tar.Jcrop({
 //      maxSize:[400,400],
-//      minSize:[100,100],
-//      boxWidth:400,
-//      bgColor:'rgb(129,129,129)',
-//      allowSelect:false,
-//      onChange: updatePreview,
-//      onSelect: updateCoords,
-//      aspectRatio: 1
-//    },function(){
-//
-//      // 图片实际宽长
-//      var bounds = this.getBounds();
-//      boundx = bounds[0];
-//      boundy = bounds[1];
-//      //绘制截取框
-//      if(tar_wid > tar_hei){
-//        this.setSelect([0,0,tar_hei,tar_hei]);
-//      }
-//      else{
-//        this.setSelect([0,0,tar_wid,tar_wid]);
-//      }
-//      // console.log(bounds);
-//      // 保存this(jcrop)到jcrop_api
-//      jcrop_api = this;
-//
-//      // Move the preview into the jcrop container for css positioning
-//      $preview.appendTo(jcrop_api.ui.holder);
-//    });
-//
-//    function updatePreview(c)
-//    {
-//      // console.log(c);
-//      if (parseInt(c.w) > 0)
-//      {
-//        //宽长放大倍数
-//        var rx = xsize / c.w;
-//        var ry = ysize / c.h;
-//        // console.log($pimg.css('width'));
-//        // console.log(boundx);
-//        // console.log(Math.round(rx * boundx) + 'px');
-//        $pimg.css({
-//          width: Math.round(rx * boundx) + 'px',
-//          height: Math.round(ry * boundy) + 'px',
-//          marginLeft: '-' + Math.round(rx * c.x) + 'px',
-//          marginTop: '-' + Math.round(ry * c.y) + 'px'
-//        });
-//        // console.log($pimg.css('width'));
-//      }
-//    };
-//    function updateCoords(c){
-//      $('#x').val(Math.round(c.x));
-//      $('#y').val(Math.round(c.y));
-//      $('#w').val(Math.round(c.w));
-//      $('#h').val(Math.round(c.h));
-//    };
-//  // });
+      minSize:[100,100],
+      boxWidth:300,
+      bgColor:'rgb(129,129,129)',
+      allowSelect:false,
+      onChange: updatePreview,
+      onSelect: updateCoords,
+      aspectRatio: 1
+    },function(){
+
+      // 图片实际宽长
+      var bounds = this.getBounds();
+      boundx = bounds[0];
+      boundy = bounds[1];
+      //绘制截取框
+      if(tar_wid > tar_hei){
+        this.setSelect([0,0,tar_hei,tar_hei]);
+      }
+      else{
+        this.setSelect([0,0,tar_wid,tar_wid]);
+      }
+      // console.log(bounds);
+      // 保存this(jcrop)到jcrop_api
+      jcrop_api = this;
+
+      // Move the preview into the jcrop container for css positioning
+      $preview.appendTo(jcrop_api.ui.holder);
+    });
+
+    function updatePreview(c)
+    {
+      // console.log(c);
+      if (parseInt(c.w) > 0)
+      {
+        //宽长放大倍数
+        var rx = xsize / c.w;
+        var ry = ysize / c.h;
+        // console.log($pimg.css('width'));
+        // console.log(boundx);
+        // console.log(Math.round(rx * boundx) + 'px');
+          //绘制预览框
+        $pimg.css({
+          width: Math.round(rx * boundx) + 'px',
+          height: Math.round(ry * boundy) + 'px',
+          marginLeft: '-' + Math.round(rx * c.x) + 'px',
+          marginTop: '-' + Math.round(ry * c.y) + 'px'
+        });
+        // console.log($pimg.css('width'));
+      }
+    }
+    //填写表单
+    function updateCoords(c){
+      $('#x').val(Math.round(c.x));
+      $('#y').val(Math.round(c.y));
+      $('#w').val(Math.round(c.w));
+      $('#h').val(Math.round(c.h));
+    }
+  // });
+};
--- js/login.js
+++ js/login.js
@@ -1,71 +1,78 @@
 /**
  * Created by zxy on 2015/6/11.
  */
-var user = $('#inputUser');
-var pass = $('#inputPassword');
-var forget = $('.form_forget');
-var comp = $('.complete');
-var form_ask = $('#form_ask');
-var ans = $('#inputAns');
-var close = $('.comp_close');
-var form_change = $('#form_change');
-var chan_pass = $('#inputPass');
-var pass_again = $('#inputPass_again');
-//弹出遮罩
-forget.bind('click',function(){
-    comp.css('display','block');
-//    form_ask.style.display = 'block';
-    form_ask.css('display','block');
-});
-//关闭遮罩
-close.each(function(){
-   $(this).bind('click',function(){
-       comp.css('display','none');
-       form_ask.css('display','none');
-       form_change.css('display','none');
-       ans.val('');
-       chan_pass.val('');
-       pass_again.val('');
-   })
-});
+var $user = $('#inputUser');
+var $pass = $('#inputPassword');
+$user.tooltip({title:'学号',trigger:'focus'});
+$user.tooltip('show');
+$pass.tooltip({title:'ucenter/重邮通行证/身份证后六位',trigger:'focus'});
+//var forget = $('.form_forget');
+//var comp = $('.complete');
+//var form_ask = $('#form_ask');
+//var ans = $('#inputAns');
+//var close = $('.comp_close');
+//var form_change = $('#form_change');
+//var chan_pass = $('#inputPass');
+//var pass_again = $('#inputPass_again');
+////弹出遮罩
+//forget.bind('click',function(){
+//    comp.css('display','block');
+////    form_ask.style.display = 'block';
+//    form_ask.css('display','block');
+//});
+////关闭遮罩
+//close.each(function(){
+//   $(this).bind('click',function(){
+//       comp.css('display','none');
+//       form_ask.css('display','none');
+//       form_change.css('display','none');
+//       ans.val('');
+//       chan_pass.val('');
+//       pass_again.val('');
+//   })
+//});
 //登陆验证
 var login_check = new form_check({
     form:'#form_login',
     check:[function(){
         var reg = /s?\d+/;
-        var value = user.val();
-        if(reg.test(value)&&value.length === 10&&pass.val() !== ''){
-            console.log('true');
-            return true;
-        }
-        else{
-            console.log('false');
+        var value = $user.val();
+        if(!reg.test(value)||value.length !== 10){
+            $user.tooltip('show');
+            $user.focus();
             return false;
         }
-    }]
-});
-//密保问题验证
-var ask_check = new form_check({
-    form:'#form_ask',
-    check:[function(){
-        if(ans.val() !== ''){
-    return true;
-}
-else{
-    return false;
-}
-}]
-});
-var change_check = new form_check({
-    form:'#form_change',
-    check:[function(){
-        if(chan_pass.val() === ''||pass_again.val() === ''){
+        else if($pass.val() === ''){
+            $pass.tooltip('show');
+            $pass.focus();
             return false;
         }
-        else{
-            return true;
-        }
+        return true;
     }]
 });
 login_check.run();
-ask_check.run();
+////密保问题验证
+//var ask_check = new form_check({
+//    form:'#form_ask',
+//    check:[function(){
+//        if(ans.val() !== ''){
+//    return true;
+//}
+//else{
+//    return false;
+//}
+//}]
+//});
+//var change_check = new form_check({
+//    form:'#form_change',
+//    check:[function(){
+//        if(chan_pass.val() === ''||pass_again.val() === ''){
+//            return false;
+//        }
+//        else{
+//            return true;
+//        }
+//    }]
+//});
+
+//ask_check.run();
